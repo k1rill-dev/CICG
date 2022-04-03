2ea00b634cb70d16d9e5880509d3b2b55021ee0d
k1rill-dev <63358980+k1rill-dev@users.noreply.github.com>
2022-04-03 13:37:35 +0300

    strahna

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app.secret_key = "dhkfblajdkvhbj"
 def sql_connection():
     try:
         con = psycopg2.connect(
-        database="postgres",
-        user="postgres",
-        password="KOKI___KLAVY123",
-        host="localhost",
+        database="d2hrrbu1311r1b",
+        user="gahdttwnarszcm",
+        password="f45246e187465ddb5c8e20b62092434faf8d59cbd6404e71d36a536a983a8af1",
+        host="ec2-18-214-134-226.compute-1.amazonaws.com",
         port="5432"
     )
         return con
